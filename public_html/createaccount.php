d9ace155fab5fc36bcf875097e89b6e05b545282
Eric Brewer <ericbrew2@gmail.com>
2022-03-22 14:05:44 -0500

    Added create account page and a hyperlink to create account on index.html

---
+++ public_html/createaccount.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html lang="en">
+<link rel="stylesheet" href="styles.css"/>
+<head>
+    <title>
+        LOGIN
+    </title>
+</head>
+<body>
+    <ul>
+        <li><a href="index.html">(Home)</a></li>
+      </ul>
+    <div id="rainbowImg">
+        <img
+          src="images/RainbowHeader.png"
+          alt="A rainbow gradient with 
+          black stripes."
+        />
+       
+      </div>
+    <div class="credentials" id="credentials">
+        <h1>Create new account</h1>
+    <form method="POST">
+        <label for="username"><b>Username: </b></label>
+        <input type="text" name="username" id="username" required minlength="1" maxlength="250"><br>
+        <label for="password"><b>Password: </b></label>
+        <input style="position: relative; left: 2px;" type="password" name="password" id="password" required minlength="1" maxlength="16"><br>
+        <label for="confirmpass"><b>Confirm Password: </b></label>
+        <input type="confirmpassword" name="confirmpassword" id="confirmpassword" required minlength="1" maxlength="16"><br>
+        <button type="submit" href="homePageLoggedIn.html">Create Account</button>
+    </form>
+      <?php
+        require("./dbConnection.php");
+        $doesUserExist = verifyUser($_POST['username'], $_POST['password']);
+        
+        if($doesUserExist){
+          header("Location: ./homePageLoggedIn.html");
+          exit;
+        }
+        else
+          echo "Username or password is incorrect.";
+      ?>
+    </div>
+</body>
+</html>
